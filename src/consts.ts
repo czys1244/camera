b10c44c32b7d50b5347833ce88fd823145b7ef40
krzysiek <krzysiekrudnik7@gmail.com>
2023-06-14 12:32:04 +0200

    adjusted textures

--- src/consts.ts
+++ src/consts.ts
@@ -12,14 +12,14 @@ export const COLOR = "magenta";
 export const COLOR_STROKE = "black";
 
 export const phongParams1: PhongParams = {
-  ka: 0.2,
-  kd: 0.4,
-  ks: 0.5,
-  ia: [255, 0, 255],
-  id: [255, 0, 255],
+  ka: 0.6,
+  kd: 0.6,
+  ks: 1,
+  ia: [246, 254, 255],
+  id: [255, 255, 255],
   is: [0, 0, 0],
-  alpha: 64,
-  m: [1, 4, 3],
+  alpha: 500,
+  m: [1, 4, 1],
   rad: 1.0,
   label: "szkło",
 };
@@ -27,14 +27,14 @@ export const phongParams1: PhongParams = {
 export const phongParams2: PhongParams = {
   ka: 0.2,
   kd: 0.4,
-  ks: 0.5,
-  ia: [155, 155, 250],
-  id: [155, 155, 250],
-  is: [0, 0, 0],
-  alpha: 64,
+  ks: 0.4,
+  ia: [250, 150, 20],
+  id: [240, 140, 15],
+  is: [255, 255, 255],
+  alpha: 1,
   m: [1, 4, 1],
   rad: 1.0,
-  label: "nwm2",
+  label: "drewno",
 };
 
 export const phongParams3: PhongParams = {
@@ -51,14 +51,14 @@ export const phongParams3: PhongParams = {
 };
 
 export const phongParams4: PhongParams = {
-  ka: 0.2,
-  kd: 0.4,
+  ka: 0.3,
+  kd: 0.2,
   ks: 0.5,
-  ia: [15, 0, 255],
-  id: [20, 10, 245],
-  is: [0, 0, 0],
-  alpha: 64,
-  m: [1, 4, 6],
+  ia: [246, 254, 255],
+  id: [255, 255, 255],
+  is: [255, 255, 255],
+  alpha: 3,
+  m: [1, 4, 2],
   rad: 1.0,
-  label: "nwm",
+  label: "stal",
 };
